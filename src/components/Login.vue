b906f0509b4e578a99c11d583cda968d108cd96a
zhaojianpeng <zhaojianpeng@ablecloud.cn>
2017-11-08 17:53:22 +0800

    add regist

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -9,6 +9,7 @@
             </el-form-item><el-form-item>
             <el-button type="primary" @click="login">登录</el-button>
             <el-button type="text" @click="reset">修改密码</el-button>
+            <el-button type="text" @click="regist">注册</el-button>
 
             </el-form-item>
         </el-form>
@@ -27,6 +28,20 @@
             <div slot="footer" class="dialog-footer">
                 <el-button @click="resetVisible = false">取 消</el-button>
                 <el-button type="primary" @click="confirmReset">确 定</el-button>
+            </div>
+        </el-dialog>
+        <el-dialog title="注册" :visible.sync="registVisible">
+            <el-form :model="registObj">
+                <el-form-item label="用户名">
+                    <el-input v-model="registObj.username"></el-input>
+                </el-form-item>
+                <el-form-item label="密码">
+                    <el-input v-model="registObj.password"></el-input>
+                </el-form-item>
+            </el-form>
+            <div slot="footer" class="dialog-footer">
+                <el-button @click="registVisible = false">取 消</el-button>
+                <el-button type="primary" @click="confirmRegist">确 定</el-button>
             </div>
         </el-dialog>
     </div>
@@ -48,10 +63,18 @@ export default {
             },
             initResetObj: {
                 username: '',
-                password: '',
-                newPassword: ''
+                password: ''
             },
-            resetVisible: false
+            registObj: {
+                username: '',
+                password: ''
+            },
+            initRegistObj: {
+                username: '',
+                password: ''
+            },
+            resetVisible: false,
+            registVisible: false
         }
     },
     methods: {
@@ -116,6 +139,42 @@ export default {
                         message: '修改成功, 请重新登录'
                     })
                     this.resetVisible = false
+                }
+            }).catch(err => {
+                this.$message({
+                    type: 'warning',
+                    message: '修改失败,请稍后重试'
+                })
+                console.log(err)
+            })
+        },
+        regist () {
+            this.registObj = Object.assign({}, this.initRegistObj)
+            this.registVisible = true
+        },
+        confirmRegist () {
+            if (!this.registObj.username) {
+                this.$message({
+                    type: 'warning',
+                    message: '请填写用户名'
+                })
+                return
+            }
+            if (!this.registObj.password) {
+                this.$message({
+                    type: 'warning',
+                    message: '请填写密码'
+                })
+                return
+            }
+            fetch('login/regist', this.registObj).then(res => {
+                console.log(res)
+                if (res.success) {
+                    this.$message({
+                        type: 'success',
+                        message: '注册成功, 请登录'
+                    })
+                    this.registVisible = false
                 }
             }).catch(err => {
                 this.$message({
